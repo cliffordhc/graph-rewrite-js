decbd20f97abfa92dd4f23c8a2f27330eeb5077d
cliffordc <cliffordc@astechportal.com>
2018-11-20 09:19:59 +0200

    export SePO class

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Graph } = require('./src/graph');
 const { Vf2 } = require('./src/vf2');
+const { SePO } = require('./src/sesqui-pushout');
 
 module.exports = {
   Graph,
   Vf2,
+  SePO,
 };
